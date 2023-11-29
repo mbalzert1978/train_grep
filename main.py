4b5e2b4bdc5db5708151d4fda43c90defb6da624
Markus Iorio <markus.iorio@ionos.com>
2023-11-29 07:47:47 +0100

    Refactor bootstrap function

--- main.py
+++ main.py
@@ -1,32 +1,17 @@
 """Main module."""
 import sys
 
-import arg_parser
-import collector
+import boot
 import commands
-import error
 import events
-import finder
-import stout_logger
-import view
-
-
-def bootstrap() -> None:
-    """Set up the handler."""
-    stout_logger.setup()
-    arg_parser.setup()
-    collector.setup()
-    finder.setup()
-    view.setup()
-    error.setup()
 
 
 def main() -> None:
     """Bootstrap."""
-    bootstrap()
     events.post_event(events.StartUp())
     commands.post_command(commands.ParseArgs(sys.argv))
 
 
 if __name__ == "__main__":
+    boot.bootstrap()
     main()
