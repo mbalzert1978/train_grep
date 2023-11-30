f38e3a1264cffe3bd50f691d1becfeeae18826bf
Markus Iorio <markus.iorio@ionos.com>
2023-11-30 07:16:29 +0100

    formating

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -9,27 +9,32 @@ import events
 
 def test_collect_lines_with_str_path():
     # Arrange
-    msg = 'test line'
+    msg = "test line"
     fp = tempfile.NamedTemporaryFile(delete_on_close=False)
     fp.write(msg.encode())
     fp.close()
-    event = events.ArgumentsParsed(path=fp.name, regex='test')
+    event = events.ArgumentsParsed(path=fp.name, regex="test")
     commands.invoke = MagicMock()
 
     # Act
     collector.collect_lines(event)
 
     # Assert
-    commands.invoke.assert_called_once_with(commands.FindLines((msg,), 'test'))
+    commands.invoke.assert_called_once_with(commands.FindLines((msg,), "test"))
 
 
 def test_collect_lines_with_file_not_found():
     # Arrange
-    event = events.ArgumentsParsed(path='nonexistent.txt', regex='test')
+    event = events.ArgumentsParsed(path="nonexistent.txt", regex="test")
     events.emit = MagicMock()
 
     # Act
     collector.collect_lines(event)
 
     # Assert
-    events.emit.assert_called_once_with(events.PathNotFoundError(message='File not found: nonexistent.txt', path=pathlib.PosixPath('nonexistent.txt')))
+    events.emit.assert_called_once_with(
+        events.PathNotFoundError(
+            message="File not found: nonexistent.txt",
+            path=pathlib.PosixPath("nonexistent.txt"),
+        )
+    )
