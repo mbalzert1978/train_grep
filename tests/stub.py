dcb61893c0a52eb5cc98c318b83a24f81ace7a59
Markus Iorio <mbalzert@gmail.com>
2024-02-06 20:13:20 +0100

    generic stub

--- tests/stub.py
+++ tests/stub.py
@@ -3,10 +3,10 @@ import typing
 T = typing.TypeVar("T")
 
 
-class CallableStub:
+class CallableStub(typing.Generic[T]):
     def __init__(self, to_raise: type[Exception] | None = None) -> None:
         self.called = False
-        self.called_with: list[typing.Any] = []
+        self.called_with: list[T] = []
         self.to_raise = to_raise
 
     def __call__(self, cmd: T) -> None:
