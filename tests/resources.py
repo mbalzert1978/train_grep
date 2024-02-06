7a1a6cdc1acf84654b4f251f0c3af1f41c5ad1d9
Markus Iorio <mbalzert@gmail.com>
2024-02-06 20:13:10 +0100

    test resources

---
+++ tests/resources.py
@@ -0,0 +1,10 @@
+PATTERN = "sun"
+NOT_FOUND_PATH = "not_found_file"
+LINES = (
+    "This is an example of a sentence.\n",
+    "Python is a popular programming language.\n",
+    "The sun is shining in the sky.\n",
+    "This is another sentence.\n",
+    "The world is full of possibilities.\n",
+)
+ARGS = ["python_file_name.py", "test file", "pattern"]
