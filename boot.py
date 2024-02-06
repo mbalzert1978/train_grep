a87b4609e6b1e49149b1a60111d0d441653f80be
Markus Iorio <mbalzert@gmail.com>
2024-02-06 21:08:19 +0100

    improve to better logging system, config check parse or whatever

--- boot.py
+++ boot.py
@@ -1,10 +1,37 @@
 """Bootstrap the application."""
+import typing
+from unittest.mock import MagicMock
+
 import handler
 
+if typing.TYPE_CHECKING:
+    import types
 
-def bootstrap() -> None:
-    """Set up the handler."""
-    handler.std_out_logger.setup()
+
+def bootstrap(logger_type: str = "std_out") -> None:
+    """
+    Bootsstrap the application.
+
+    Parameters
+    ----------
+    logger_type : str, optional
+        _description_, by default "std_out"
+    valid options:
+        `std_out`, `file` or `none`
+
+    """
+    log_module: types.ModuleType
+    match logger_type:
+        case "std_out":
+            log_module = handler.std_out_logger
+        case "file":
+            log_module = handler.file_logger
+        case "none":
+            log_module = MagicMock()
+        case _:
+            log_module = handler.std_out_logger
+
+    log_module.setup()
     handler.parser.setup()
     handler.file_handler.setup()
     handler.finder.setup()
