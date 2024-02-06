38506a55acecf40e3d41c986129554ecc0d85c71
Markus Iorio <mbalzert@gmail.com>
2024-02-06 16:07:51 +0100

    better test sentences

--- tests/test_finder.py
+++ tests/test_finder.py
@@ -7,10 +7,16 @@ from tests.stub import CallableStub
 def test_finder_finds_all_lines(register) -> None:
     # Arrange
     stub: CallableStub = register(CallableStub(), events.LinesCollected)
-    lines = ("Dies ist ein,", "test file", "das bestimmte", "test worte endhaelt")
+    lines = (
+        "This is an example of a sentence.",
+        "Python is a popular programming language.",
+        "The sun is shining in the sky.",
+        "This is another sentence.",
+        "The world is full of possibilities.",
+    )
 
     # Act
-    handler.finder.search_pattern(commands.FindLines(lines, "test"))
+    handler.finder.search_pattern(commands.FindLines(lines, "sun"))
 
     # Assert
-    assert list(stub.called_with.pop().found) == ["test file", "test worte endhaelt"]
+    assert list(stub.called_with.pop().found) == ["The sun is shining in the sky."]
