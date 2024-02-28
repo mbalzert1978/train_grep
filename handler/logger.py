f38d7b748d4ec041428f46331f66b364fd69f2ce
mbalzert1978 <61972572+mbalzert1978@users.noreply.github.com>
2024-02-28 10:27:02 +0000

    remove poetry and logging murphy

--- handler/logger.py
+++ handler/logger.py
@@ -1,12 +1,10 @@
 """Log events to stdout."""
-import atexit
 import dataclasses
 import logging
 import logging.config
 import sys
 
 import events
-from resources import default, string
 
 LEVEL = logging.INFO
 STREAM = sys.stdout
@@ -26,18 +24,9 @@ def handle_error(err_event: events.Error) -> None:
 
 def setup() -> None:
     """Register the logger events."""
-    _setup_logging()
     for event in events.Event.__subclasses__():
         events.register(event, handle_info)
     for err_event in events.Error.__subclasses__():
         events.register(err_event, handle_error)
 
 
-def _setup_logging() -> None:
-    logging.config.dictConfig(default.LOGGING_CONFIG)
-    match logging.getHandlerByName("queue_handler"):
-        case logging.Handler() as queue_handler:
-            queue_handler.listener.start()  # type: ignore
-            atexit.register(queue_handler.listener.stop)  # type: ignore
-        case _:
-            logging.basicConfig(format=string.FORMAT, level=LEVEL, stream=STREAM)
